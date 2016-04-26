a059f0389c858e81e4f7f4f854b479995e154b7a
Simon Whelan <simon.whelan.evolution@gmail.com>
2016-04-26 16:00:58 +0200

    Small edit to fix stdout

--- process.cxx
+++ process.cxx
@@ -2915,7 +2915,7 @@ CCodonProcess::CCodonProcess(CData *D, CTree *T, CodonProc Model, ECodonEqm CE, 
 	// Do the model equilibrium distribution
 	AddCodonEqm(GenCode,m_pData->m_iChar,CE,false);
 	// Define the model parameters
-	cout << "\nAdding codon process" << flush;
+#	cout << "\nAdding codon process" << flush;
 
 	if(Model == pM0DrDc) {
 		cout << "\nMaking random Radical matrix -> <Random.mat>";
